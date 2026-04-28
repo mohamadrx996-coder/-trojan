a05a741d9ab2d41ed37e6b315b63795cf77e1d56
mohamadrx996-coder <mohamadrx996@gmail.com>
2026-04-28 22:05:26 +0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['sharp'],
+  },
+};
+
+export default nextConfig;
